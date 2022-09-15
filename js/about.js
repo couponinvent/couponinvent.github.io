82e8dcd67d242a7901d308cbddc7022aefdc7f06
Shivam Goel <shivgoel@adobe.com>
2022-09-15 22:40:55 +0530

    deleted files

--- js/about.js
+++
@@ -1,6 +0,0 @@
-// About CN
-$('.about-cn-container').on('click', e => {
-  openInNewTab('https://www.codingninjas.com/about?referralCode=TNMBH');
-});
-
-//FIXME: Zyada Scroll Ho rha h
--- js/slider-animation-try.js
+++
@@ -1,9 +0,0 @@
-$('#dp-prev-btn').click(()=>{
-    $('.discount-img-cntr-1').addClass("animate__animated animate__slideOutLeft");
-    $('.discount-img-cntr-2').addClass("animate__animated animate__slideOutLeft");
-})
-
-$('#dp-next-btn').click(()=>{
-    $('.discount-img-cntr-1').addClass("animate__animated animate__slideOutRight");
-    $('.discount-img-cntr-2').addClass("animate__animated animate__slideOutRight");
-})
